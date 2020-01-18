bc0e4bd937db4bdbab9b67496339ebff43542769
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2020-01-18 22:12:37 +0300

    Create a progress bar

--- game.js
+++ game.js
@@ -4,8 +4,9 @@ const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 
 // our hud reference to javascript
-const questionCounterText = document.getElementById("questionCounter");
+const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
+const ProgressBarFull = document.getElementById("ProgressBarFull");
 
 // create variables
 let currentQuestion = {};
@@ -60,7 +61,9 @@ getNewQuestion = () => {
     return window.location.assign("/end.html");
   }
   questionCounter++;
-  questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+  progressText.innerText = ` Question ${questionCounter}/${MAX_QUESTIONS}`;
+  // Update the progress bar
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
